26cae0925211b0181b69ac7c4beb42bb91728c30
umeaclinton <michaeludochukwu99@gmail.com>
2026-07-11 19:09:17 +0100

    feat: initialize Tailwind CSS configuration with default project directory paths

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { config } from "tailwindcss";
+
+const config: config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: [],
+}
+
+export default config;
